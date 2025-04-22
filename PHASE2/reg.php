b0cd0722002fbac36cf547d55eccacb3e4fab7d3
habijey <habilanjeyarasa@gmail.com>
2025-04-22 19:46:42 +0200

    Update reg.php

--- PHASE2/reg.php
+++ PHASE2/reg.php
@@ -107,7 +107,7 @@ if (isset($_POST['submit'])) {
             file_put_contents('users/' . $email . '.txt', $contenu);
 
             $_SESSION['email'] = $email;
-            header('Location: main.php');
+            header('Location: accueil.html');
             exit();
         }
     }
