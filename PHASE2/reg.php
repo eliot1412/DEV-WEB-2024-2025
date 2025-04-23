e5dbd4ae9eeb71b5bfbe63dbb36e7c7a29cb7422
Eliot <eliot.durand-de-gevigney@etu.cyu.fr>
2025-04-23 21:01:25 +0200

    Update reg.php

--- PHASE2/reg.php
+++ PHASE2/reg.php
@@ -16,26 +16,32 @@ session_start();
 <body>
 
 <div class="head">
-        
+
         <ul>
             <a href="accueil.php">
                 <img src="VolcanFly.jpg" alt="Accueil">
             </a>
-            
+
         </ul>
         <div class="headers">
-            
+
             <ul>
                 <li><a href="accueil.php">Accueil</a></li>
                 <li><a href="reg.php">Inscription</a></li>
                 <li><a href="log.php">Connexion</a></li>
                 <li><a href="choice.php">Voyages</a></li>
                 <li><a href="aides.php">Aides</a></li>
-                
-            </ul>
-        </div>
-    </div>
 
+            </ul>
+
+
+
+        </div>
+
+        <a href="profile.php">
+            <img src="pp.jpg" alt="profile">
+            </a>
+    </div>
 <div class="Centre">
     <div class="title">
         <h1>Inscrivez-vous pour profiter du meilleur de VolcanFly</h1>
