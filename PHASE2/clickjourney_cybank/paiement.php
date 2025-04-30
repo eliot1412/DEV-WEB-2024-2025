55e049435a718d331dfdc793c69f0b3542c4773a
habijey <habilanjeyarasa@gmail.com>
2025-04-30 07:23:24 +0200

    Update paiement.php

--- PHASE2/clickjourney_cybank/paiement.php
+++ PHASE2/clickjourney_cybank/paiement.php
@@ -1,29 +1,9 @@
-<?php
-session_start();
-require('getapikey.php');
-
-$transaction = uniqid();
-$montant = $_POST['montant'];
-$vendeur = 'MI-1_A'; // À adapter à votre groupe
-$retour = "http://localhost/retour_paiement.php?session=" . session_id();
-
-$api_key = getAPIKey($vendeur);
-$control = md5($api_key . "#" . $transaction . "#" . $montant . "#" . $vendeur . "#" . $retour . "#");
-
-$_SESSION['cybank'] = [
-    'transaction' => $transaction,
-    'montant' => $montant,
-    'vendeur' => $vendeur,
-    'retour' => $retour,
-    'control' => $control,
-];
-?>
-
+<h2>Redirection vers CYBank</h2>
 <form action="https://www.plateforme-smc.fr/cybank/index.php" method="POST">
   <input type="hidden" name="transaction" value="<?= $transaction ?>">
   <input type="hidden" name="montant" value="<?= $montant ?>">
   <input type="hidden" name="vendeur" value="<?= $vendeur ?>">
   <input type="hidden" name="retour" value="<?= $retour ?>">
   <input type="hidden" name="control" value="<?= $control ?>">
-  <input type="submit" value="Valider et payer sur CYBank">
+  <input type="submit" value="Payer avec CYBank">
 </form>
