7cebd3b6e09059b0741c429239186a88131d2e92
habijey <habilanjeyarasa@gmail.com>
2025-05-01 09:33:13 +0200

    Update paiement.php

--- PHASE2/clickjourney_cybank/paiement.php
+++ PHASE2/clickjourney_cybank/paiement.php
@@ -1,4 +1,27 @@
-<h2>Redirection vers CYBank</h2>
+<?php
+require('getapikey.php');
+
+if (!isset($_POST['montant'])) {
+    die("Erreur : montant non fourni");
+}
+
+$transaction = substr(md5(uniqid()), 0, 12); 
+$montant = number_format((float)$_POST['montant'], 2, '.', '');
+
+$vendeur = 'MI-2_D'; 
+$retour = 'http://localhost/retour_paiement.php?session=test123';
+
+
+$api_key = getAPIKey($vendeur);
+if ($api_key === 'zzzz') {
+    die("Erreur : vendeur invalide");
+}
+
+
+$control = md5($api_key . "#" . $transaction . "#" . $montant . "#" . $vendeur . "#" . $retour . "#");
+
+?>
+
 <form action="https://www.plateforme-smc.fr/cybank/index.php" method="POST">
   <input type="hidden" name="transaction" value="<?= $transaction ?>">
   <input type="hidden" name="montant" value="<?= $montant ?>">
