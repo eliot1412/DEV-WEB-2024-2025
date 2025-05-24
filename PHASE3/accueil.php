d988724a7d70ab1b902e3002189192ddf64b85ec
habijey <habilanjeyarasa@gmail.com>
2025-05-24 16:54:51 +0200

    Update accueil.php

--- PHASE3/accueil.php
+++ PHASE3/accueil.php
@@ -5,56 +5,50 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="head.css">
     <link rel="stylesheet" type="text/css" href="accueil.css">
-    
+    <script src="theme.js" defer></script>
 </head>
 
 <body>
-<div class="head">
-
+    <div class="head">
         <ul>
             <a href="accueil.php">
                 <img src="VolcanFly.jpg" alt="Accueil">
             </a>
-
         </ul>
+        
         <div class="headers">
-
             <ul>
                 <li><a href="accueil.php">Accueil</a></li>
                 <li><a href="reg.php">Inscription</a></li>
                 <li><a href="log.php">Connexion</a></li>
                 <li><a href="choice.php">Voyages</a></li>
                 <li><a href="aides.php">Aides</a></li>
-
+                <li><a href="panier.php">Panier</a></li>
             </ul>
-
-
-
         </div>
 
         <a href="profile.php">
             <img src="pp.jpg" alt="profile">
-            </a>
+        </a>
     </div>
 
-   
-
     <div class="Centre">
-        <pre> 
+        <video autoplay muted loop class="video-background">
+            <source src="volcan-night.mp4" type="video/mp4">
+        </video>
+        <pre class="centre-text"> 
 Bienvenue sur le site de notre agence de voyage VolcanFly!
 Nous vous offrons les meilleurs offres et destinations pour visiter les plus beaux volcan du monde!
 Nos services assurerons votre sécurité et votre bien-être tout au long du séjour!
 Si vous êtes intrépide, rejoignez l'aventure!
-
         </pre>
     </div>
-    <div></div>
 
     <div class="tail">
-    <a href="accueil.php">
-        <p>Accueil</p>
-    </a>
-    <p>| Destinations | Offres spéciales | Contact | À propos</p>
-</div>
+        <a href="accueil.php">
+            <p>Accueil</p>
+        </a>
+        <p>| Destinations | Offres spéciales | Contact | À propos</p>
+    </div>
 </body>
 </html>
